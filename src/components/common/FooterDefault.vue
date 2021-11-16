115a4f02c0eae25cf0fc39438d405a3a9a27c266
raymoond0306 <raymoond0306@gmail.com>
2021-11-16 18:34:00 +0300

    Project First upload

---
+++ src/components/common/FooterDefault.vue
@@ -0,0 +1,13 @@
+<template>
+	<footer class="footer">
+		<div class="container">
+			<div class="footer-bottom">
+				<div class="footer-left"></div>
+				<div class="footer-center">
+					<p class="copyright">Vue Next</p>
+				</div>
+				<div class="footer-right"></div>
+			</div>
+		</div>
+	</footer>
+</template>
